09b31fe19d0d579f3722fbf9bd36dbc27b1bb238
Moscolape <chunkums088@gmail.com>
2025-03-06 09:22:58 +0100

    chore: setup backend server

---
+++ app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const contactRoutes = require("./routes/contactRoutes");
+
+const app = express();
+app.use(express.json());
+
+const allowedOrigins = ["http://localhost:5174", "https://akpoazaafoundation.org"];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin); // ✅ Allow the request if the origin is in the list
+    } else {
+      callback(new Error("Not allowed by CORS")); // ❌ Reject if not in the list
+    }
+  },
+  methods: ["POST", "GET", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
+
+// Use routes
+app.use("/api", contactRoutes);
+
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
